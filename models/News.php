389697a17c235ce1d192dde387fd699885493cf7
krivbass3D <33382464+krivbass3D@users.noreply.github.com>
2021-01-17 21:38:20 +0200

    создает новостей

---
+++ models/News.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "news".
+ *
+ * @property int $id
+ * @property string $title
+ * @property string|null $text
+ * @property string $url
+ * @property int $status_id
+ * @property int $sort
+ */
+class News extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'news';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['title', 'url'], 'required'],
+            [['text'], 'string'],
+            [['status_id', 'sort'], 'integer'],
+            [['title', 'url'], 'string', 'max' => 150],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => 'Title',
+            'text' => 'Text',
+            'url' => 'Url',
+            'status_id' => 'Status ID',
+            'sort' => 'Sort',
+        ];
+    }
+}
